893c6d43167078779aa686dfea7a8d1fa57dcd74
Danny Verdel <dannyverdel@Dannys-MacBook-Pro.local>
2023-02-21 15:35:36 +0100

    initialized project and setup gitignore

---
+++ TheChernoExerciseFiles/TheChernoExerciseFiles/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TheChernoExerciseFiles
+//
+//  Created by Danny Verdel on 21/02/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
